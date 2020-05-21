30aa247189ab959ca264ce17e4bde1dba361ecd6
mateuszpmroz <mateuszpmroz@gmail.com>
2020-05-21 12:52:42 +0200

    dockerize the app and startup the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { KeyGeneratorPage } from './components/pages/keyGenerator/KeyGeneratorPage';
+
+function App() {
+  return (
+    <div className="App">
+      <KeyGeneratorPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/keyGenerator/KeyGeneratorPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const KeyGeneratorPage = () => {
+  return (
+    <div>
+      Online key generator
+    </div>
+  )
+}
