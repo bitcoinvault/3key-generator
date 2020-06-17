ded895ddf730284b428a61ace278a252905e4356
mateuszpmroz <mateuszpmroz@gmail.com>
2020-06-17 15:38:45 +0200

    add pdf design

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,3 +1,7 @@
 export const images = {
   logo: require('./images/logo.png'),
 };
+
+export const pdf = {
+  header: require('./pdf/header.png'),
+}
--- src/components/pages/keyGenerator/KeyGeneratorPdf.tsx
+++ src/components/pages/keyGenerator/KeyGeneratorPdf.tsx
@@ -2,55 +2,91 @@ import React from 'react';
 import { Document, Page, Text, View, StyleSheet, Image, Font } from '@react-pdf/renderer';
 import { KeyGenerator } from '../keyGenerator/KeyGeneratorContent';
 import { en } from '../../../locale/en';
+import { pdf } from 'assets';
 
 Font.register({ family: 'Ubuntu', src: '/fonts/Ubuntu-Regular.ttf' });
+Font.register({ family: 'Ubuntu-Bold', src: '/fonts/Ubuntu-Bold.ttf' });
+Font.register({ family: 'Ubuntu-Light', src: '/fonts/Ubuntu-Light.ttf' });
 
 const styles = StyleSheet.create({
   body: {
-    paddingTop: 15,
+    paddingTop: 55,
     paddingBottom: 15,
-    paddingHorizontal: 10,
+    paddingHorizontal: 32,
     fontFamily: 'Ubuntu'
   },
   title: {
-    fontSize: 24,
+    fontSize: 26,
     textAlign: 'center',
+    fontFamily: 'Ubuntu-Bold',
+    marginBottom: 16,
+  },
+  titleContainer: {
+    marginBottom: 60
   },
   subtitle: {
     fontSize: 18,
-    margin: 12,
-    textAlign: 'center',
+    fontFamily: 'Ubuntu-Bold',
+    alignSelf: 'center',
+    marginBottom: 32,
   },
   text: {
     fontSize: 12,
+    textAlign: 'center',
+    border: 1,
+    borderColor: 'rgb(204, 204, 204)',
+    borderRadius: 20,
+    marginBottom: 73,
+    paddingVertical: 16,
+  },
+  caption: {
+    fontFamily: 'Ubuntu-Light',
+    fontSize: 14,
+    textAlign: 'center',
+    marginBottom: 16,
+    color: 'rgb(148, 149, 149)'
   },
   chip: {
-    fontSize: 12,
+    fontSize: 10,
     padding: 8,
     backgroundColor: 'rgb(234, 234, 234)',
     borderRadius: 4,
     marginBottom: 16,
     marginRight: 8,
     marginLeft: 8,
-    width: '100px',
+    width: 'auto',
+    fontWeight: 'bold'
   },
   image: {
     textAlign: 'center',
     justifyContent: 'center',
-    marginVertical: 15,
-    marginHorizontal: 200
+    marginHorizontal: 50,
+  },
+  imageContainer: {
+    marginTop: 55
   },
   chipContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    fontFamily: 'Ubuntu-Bold'
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    flexGrow: 1,
+  },
+  column: {
+    flexGrow: 0,
+    flexBasis: 300,
+    flexShrink: 1,
   }
 });
 
 const breakWord = (word: string) => {
   let words = null;
-  if (word.length > 90) {
-    words = word.match(/.{1,86}/g)
+  if (word.length > 30) {
+    words = word.match(/.{1,30}/g)
   }
   return words || [word];
 }
@@ -59,18 +95,34 @@ export const KeyGeneratorPdf = (props: KeyGenerator) => {
   const { publicKey, publicKeyImgUrl, privateKeyImgUrl, words } = props;
   return (
     <Document>
-      <Page size="A4" style={styles.body} wrap>
-        <Text style={styles.title}>{en.pdf.title}</Text>
-        <View>
-          <Text style={styles.subtitle}>{en.pdf.publicKey}</Text>
-          <Text style={styles.text}>{breakWord(publicKey).map((word) => `${word}\n`)}</Text>
-          <Image style={styles.image} src={publicKeyImgUrl} />
-        </View>
-        <View>
-          <Text style={styles.subtitle}>{en.pdf.privateKey}</Text>
-          <Image style={styles.image} src={privateKeyImgUrl} />
-          <View style={styles.chipContainer}>
-            {words.map((word: string, index: number) => (<Text key={index} style={styles.chip}>{index + 1}. {word}</Text>))}
+      <Page size="A4" wrap>
+        <Image src={pdf.header} />
+        <View style={styles.body}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>{en.pdf.title}</Text>
+            <Text style={styles.caption}>{en.pdf.nextSteps}</Text>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.column}>
+              <Text style={styles.subtitle}>{en.pdf.publicKey}</Text>
+              <Text style={styles.caption}>{en.content.multiline}</Text>
+              <Text style={styles.text}>{breakWord(publicKey).map((word) => `${word}\n`)}</Text>
+              <View style={styles.imageContainer}>
+                <Text style={styles.caption}>{en.content.publicKeyQrCode}</Text>
+                <Image style={styles.image} src={publicKeyImgUrl} />
+              </View>
+            </View>
+            <View style={styles.column}>
+              <Text style={styles.subtitle}>{en.pdf.privateKey}</Text>
+              <Text style={styles.caption}>{en.content.phrase}</Text>
+              <View style={styles.chipContainer}>
+                {words.map((word: string, index: number) => (<Text key={index} style={styles.chip}>{index + 1}. {word}</Text>))}
+              </View>
+              <View style={styles.imageContainer}>
+                <Text style={styles.caption}>{en.content.privateKeyQrCode}</Text>
+                <Image style={styles.image} src={privateKeyImgUrl} />
+              </View>
+            </View>
           </View>
         </View>
       </Page>
