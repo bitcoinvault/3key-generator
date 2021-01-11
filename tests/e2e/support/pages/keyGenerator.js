b53f657feec30e57ff7c5ff1e4f08d7520165c6a
oskarbogucki <73473037+oskarbogucki@users.noreply.github.com>
2021-01-11 13:30:25 +0100

    test adeed

---
+++ tests/e2e/support/pages/keyGenerator.js
@@ -0,0 +1,16 @@
+const keyGenerator = {
+  header: '.header > .label',
+  logo: '[alt="bitcoin vault logo"]',
+  title: 'h2',
+  languageButton:
+  '[role="button"][aria-haspopup="listbox"]',
+  phrase: '.chip-container > :nth-child(1n+0)',
+  multiline: 'textarea',
+  qrPublicKey: '[alt="Public key"]',
+  qrPrivateKey: '.private-container > .qrcode-container > img',
+  getNewKeyButton: 'div.flat-button h4.styled-text',
+  exportPdfButton: '.button',
+  allRightReserved: 'h5',
+};
+
+export default keyGenerator;
